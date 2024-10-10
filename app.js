0df8fe84535a7296e5e801dcc79bbfac6a5df410
jeonsuwon <swjeon0330@gmail.com>
2024-10-10 14:45:46 +0900

    add : connect prisma

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ import express from 'express';
 import { ENV_KEY } from './constants/env.constants.js';
 
 const app = express();
-const port = 3306;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api', (req, res) => {
+  res.status(200).send({ message: '테스트에 성공하셧습니다.' });
 });
 
 app.listen(ENV_KEY.PORT, () => {
